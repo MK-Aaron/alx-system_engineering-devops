e0c76fda166da19fef5b6f1009995bd4aee24a1c
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-08-07 06:17:57 -0700

    Use internal class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    //click event handler
+    $('#red_header').click(function(){
+        //change text color of header
+        $('header').addClass('red');
+    });    
+});
