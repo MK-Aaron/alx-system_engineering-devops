b17da4f0fcc3f512f7be25396d065d35e5c60607
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-08-07 04:31:55 -0700

    Use querry selector

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+// Select the <header> element using document.querySelector
+const header = document.querySelector('header');
+
+// Update the text color to red (#FF0000)
+if (header) {
+    header.style.color = '#FF0000';
+}
