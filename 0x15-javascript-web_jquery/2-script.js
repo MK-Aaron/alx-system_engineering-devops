970d4d577af396c9177004161c479643c8261ccc
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-08-07 05:54:04 -0700

    click event handler

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+// click to turn red
+$(document).ready(function() {
+    //click event handler
+    $("#red_header").click(function(){
+        //change text color of header
+        $('header').css('color', '#FF0000');
+    });    
+});
